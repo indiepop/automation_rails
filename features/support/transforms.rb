62f5c4df6a1fbf1b7def05912b9ff8d38caf50f7
jyang2 <josh.yangzhuo@gmail.com>
2012-06-04 10:22:10 +0800

    0604|success

--- features/support/transforms.rb
+++
@@ -1,9 +0,0 @@
-
-
-USING_CREDENTIAL= Transform /(.+) credential$/  do |credential_type|
-
- table(%{
-     |form_email    | #{$info[credential_type]['username']} |
-     |form_password | #{$info[credential_type]['password']} |
-    })
- end
