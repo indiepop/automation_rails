163f3c61f81093375f850c441bae133ce52d71f8
josh <josh.yangzhuo@gmail.com>
2013-12-18 23:10:27 -0800

    after presentation

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -7,16 +7,14 @@ Given /^I Open main page using (#{USING_CREDENTIAL})$/ do  |table|
   end                                                                    #initial browser
 
 
-#  $browser.each do |browser|
-#  end
+#  $threads.each do
       table.rows_hash.each do |key,value|
       steps %{And I type '#{value}' in '#{key}' field}
-      #And  /^I type '#{value}' in '#{key}' field$/
+      And  /^I type '#{value}' in '#{key}' field$/
       #  "I type '#{value}' in '#{key}' field"                                #TODO   :fix the  warning
       end
-
-  $browser.find_element(:xpath => "//input[@class='bn-submit']").click     #set the username and password
-
+      $browser.find_element(:xpath => "//input[@class='bn-submit']").click     #set the username and password
+ # end
 end
 
 
