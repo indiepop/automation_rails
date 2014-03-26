55dc53cffe7f16e3053c20d2dbc273dc6faf9406
josh <josh.yangzhuo@gmail.com>
2014-03-26 00:04:33 -0700

    change some comment

--- lib/core/Initialization.rb
+++ lib/core/Initialization.rb
@@ -3,7 +3,7 @@
 class Initialization
   def self.start
 
-      if  FileTest::exist?("#{$root}/lib/resource/execute_ip.yml")    #多计算机执行环境
+      if  FileTest::exist?("#{$root}/lib/resource/execute_ip.yml")    #远程计算机执行环境
                 $browser = SeleniumUtils.for :remote,:url=>"http://#{$is_remote[:checked_ip] }:4444/wd/hub",:desired_capabilities => ($info['browser'].to_sym )
                 $browser.maximize_window
                 $browser.navigate.to $info['server']
